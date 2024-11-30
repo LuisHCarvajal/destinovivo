cd2793c5b34e510aec58e6b41953c20a01b22462
luis herney carvajal <luishcarvajalm@gmail.com>
2024-11-30 01:31:34 -0500

    Cambio en controller, service y repository de tablas Servicio, Tipo_Empresa y Tipo_Por_Empresa

--- proyectoDestinoVivo/src/main/java/com/proyectoDestinoVivo/proyectoDestinoVivo/controller/UsuarioController.java
+++ proyectoDestinoVivo/src/main/java/com/proyectoDestinoVivo/proyectoDestinoVivo/controller/UsuarioController.java
@@ -1,22 +1,24 @@
 package com.proyectoDestinoVivo.proyectoDestinoVivo.controller;
 
 import com.proyectoDestinoVivo.proyectoDestinoVivo.model.Usuario;
+import com.proyectoDestinoVivo.proyectoDestinoVivo.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.proyectoDestinoVivo.proyectoDestinoVivo.service.UsuarioService;
 
 import java.util.List;
 
 @RestController
 public class UsuarioController {
+    @Autowired
     private final UsuarioService usuarioService;
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
-    @PostMapping("/insertar")
+    @PostMapping("/insertarUsuario")
     public Usuario insertar(@RequestBody Usuario usuario){
         try {
             return usuarioService.insertarUsuario(usuario);
@@ -25,7 +27,7 @@ public class UsuarioController {
         }
 
     }
-    @GetMapping("/consultar")
+    @GetMapping("/consultarUsuario")
     public List<Usuario> consultar(){
         try{
             return usuarioService.consultarUsuario();
