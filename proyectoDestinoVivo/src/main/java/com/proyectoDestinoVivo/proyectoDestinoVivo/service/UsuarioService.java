1ce39bb3ea179f7e80c41aa40e641f95f9e4f69c
beto <german.rdz@territorium.com>
2024-12-09 08:30:21 -0500

    revirtiendo cambios de luis desde el master

--- proyectoDestinoVivo/src/main/java/com/proyectoDestinoVivo/proyectoDestinoVivo/service/UsuarioService.java
+++ proyectoDestinoVivo/src/main/java/com/proyectoDestinoVivo/proyectoDestinoVivo/service/UsuarioService.java
@@ -1,16 +1,15 @@
 package com.proyectoDestinoVivo.proyectoDestinoVivo.service;
+
 import com.proyectoDestinoVivo.proyectoDestinoVivo.model.Usuario;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import com.proyectoDestinoVivo.proyectoDestinoVivo.repository.UsuarioRepository;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Service
 public class UsuarioService {
-    @Autowired
     private UsuarioRepository usuarioRepository;
 
     public Usuario insertarUsuario(Usuario usuario) {
@@ -19,31 +18,6 @@ public class UsuarioService {
         } catch (DataAccessException e) {
             throw new RuntimeException("Error al insertar el usuario" + e);
         }
-    }
-
-    public List<Usuario> consultarUsuario() {
-        try {
-            List<Usuario> usuario = new ArrayList<Usuario>();
-            usuario.addAll(usuarioRepository.findAll());
-            return usuario;
-        } catch (DataAccessException e) {
-            throw new RuntimeException("Error al consultar el usuario" + e);
-        }
-    }
-   public Usuario editarUsuario(int documento, Usuario usuario){
-        Usuario existente = usuarioRepository.findById(documento).orElseThrow(()->new RuntimeException("Empresa no existente"));
-        existente.setNombre(usuario.getNombre());
-        existente.setCorreo(usuario.getCorreo());
-        existente.setTelefono(usuario.getTelefono());
-        existente.setNacionalidad(usuario.getNacionalidad());
-        existente.setFecha_nacimiento(usuario.getFecha_nacimiento());
-        return usuarioRepository.save(existente);
-    }
-
-    public String eliminarUsuario(int documento){
-        Usuario existente = usuarioRepository.findById(documento).orElseThrow(()->new RuntimeException("Empresa no existente"));
-        usuarioRepository.deleteById(existente.getDocumento());
-        return "Usuario eliminado correctamente";
     }
 
 
