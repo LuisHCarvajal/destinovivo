45175114349ec5208c3b30f115a7eb0c51dae270
beto <german.rdz@territorium.com>
2024-12-09 17:17:48 -0500

    Se agrega archivo Webconfig

--- proyectoDestinoVivo/src/main/java/com/proyectoDestinoVivo/proyectoDestinoVivo/controller/UsuarioController.java
+++ proyectoDestinoVivo/src/main/java/com/proyectoDestinoVivo/proyectoDestinoVivo/controller/UsuarioController.java
@@ -3,16 +3,14 @@ package com.proyectoDestinoVivo.proyectoDestinoVivo.controller;
 import com.proyectoDestinoVivo.proyectoDestinoVivo.model.Usuario;
 import com.proyectoDestinoVivo.proyectoDestinoVivo.service.UsuarioService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
